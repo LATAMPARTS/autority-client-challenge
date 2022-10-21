09617f695485c3cf313d89d6a6ae3a807eb457df
David Paucar <soydavidpaucar@gmail.com>
2022-10-20 20:41:35 -0500

    :art: added providers to main component to handle app data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux';
+import type { AppProps } from 'next/app';
 
-import store from '../app/store'
+import { store } from '../redux/store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<Component {...pageProps} />
+			</Provider>
+		</QueryClientProvider>
+	);
 }
